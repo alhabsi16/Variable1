8c129967a374b9111a5ae198860f94f0148fdcc8
alhabsi16 <mxmx8283@gmail.com>
2023-05-31 14:22:29 +0400

    uppVariable

--- Program.cs
+++ Program.cs
@@ -1,24 +1,43 @@
-﻿namespace Variable1
+﻿using System.Collections.Concurrent;
+
+namespace Variable1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int a = 3;
+            /*int a = 3;
             char x = 'a';
             double p = 5.5;
             string o = "hello";
-            bool b = false;
-
+            bool b = false;*/
             //Console.WriteLine(a);
             //Console.WriteLine(x);
             //Console.WriteLine(o);
             //Console.WriteLine(b);
             //Console.WriteLine(p);
-            Console.WriteLine( x + "      " + a);
-            Console.WriteLine( x+ "      " + a + "      " + p  );
+            // Console.WriteLine( x + "      " + a);
+            // Console.WriteLine( x+ "      " + a + "      " + p  );
+            // Console.WriteLine("Ans=  " + a * p);
 
-           // Console.WriteLine("Ans=  " + a * p);
+
+            //Q2: write a program to swap ?
+            int a = 5;
+            int b = 3;
+            (a, b) = (b, a);
+            Console.WriteLine(" a = " + a);
+            Console.WriteLine(" b = " + b);
+
+            //**************************
+           
+
+            string c = "Hello";
+            string d = "World";
+            (c , d) = (d, c);
+
+            Console.WriteLine("*****************************");
+            Console.WriteLine(c);
+            Console.WriteLine(d);
         }
     }
 }
