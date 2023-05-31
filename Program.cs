1e16aef1105b052f6e51f654e246fff52d40d4c9
alhabsi16 <mxmx8283@gmail.com>
2023-05-31 13:42:33 +0400

    connect

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Variable1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            char x = 'a';
+            double p = 5.5;
+            string o = "hello";
+            bool b = false;
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(x);
+            //Console.WriteLine(o);
+            //Console.WriteLine(b);
+            //Console.WriteLine(p);
+            Console.WriteLine( x + "      " + a);
+            Console.WriteLine( x+ "      " + a + "      " + p  );
+
+           // Console.WriteLine("Ans=  " + a * p);
+        }
+    }
+}
